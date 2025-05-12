3efa44c353d0394518510eedb9da340227030044
chrislush <133811141+chrislush@users.noreply.github.com>
2025-05-12 11:59:46 +0100

    Added sample route for API call.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-//import cors from 'cors';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/hello', (_req, res) => {
