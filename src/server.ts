7ba8430cdfa9b1256810a9d2f78572c2f949cad7
nchua3012 <n.chua3012@googlemail.com>
2025-05-15 13:50:58 +0100

    Test route added to test connection with Supabase DB

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,6 @@
-import express from 'express';
-import cors from 'cors';
+import app from './app.ts';
 
-const app = express();
 const PORT = 5000;
-
-app.use(cors());
-app.use(express.json());
-
-import { userRoutes } from './routes/userRoutes.ts';
-
-app.use('/users', userRoutes);
-
-app.get('/api/hello', (_req, res) => {
-  res.json({ message: 'Hello from the backend!' });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
