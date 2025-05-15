f712a79e97e4d73d20cd3a82a28ee4403a54c084
nchua3012 <n.chua3012@googlemail.com>
2025-05-15 14:29:39 +0100

    More prettier errors resolved

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { supabase } from '../config/supabaseClient.ts';
 
-export async function createUser(
-  req: Request,
-  res: Response,
-  next?: NextFunction
-): Promise<void> {
+export async function createUser(req: Request, res: Response, next?: NextFunction): Promise<void> {
   try {
     const { name, email } = req.body;
 
-    const { data, error } = await supabase
-      .from('Users')
-      .insert([{ name, email }])
-      .select();
+    const { data, error } = await supabase.from('Users').insert([{ name, email }]).select();
 
     if (error) {
       console.error('❌ Supabase insert error:', error);
