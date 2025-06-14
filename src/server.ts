e51f6ac469a4c739624c67d00ee926bf211c92fc
annavanwingerden <annavanwingerden@outlook.com>
2025-06-14 23:19:35 +0100

    prettier

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ dotenv.config();
 
 import app from './app.ts';
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
