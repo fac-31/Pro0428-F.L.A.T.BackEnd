a19b58fefa5c99ccc5b8c840e90044152c78dcb2
Anna van Wingerden <91270891+annavanwingerden@users.noreply.github.com>
2025-06-15 10:07:06 +0100

    Add Join House Endpoint and Token Refresh Improvements (#44)
    * token-fixed-saving-to-db
    
    * join-house-db
    
    * prettier

--- src/routes/houseRoutes.ts
+++ src/routes/houseRoutes.ts
@@ -3,9 +3,11 @@ const router = express.Router();
 
 import { createHouse } from '../controllers/createhouseController.ts';
 import { fetchHouseInfo, fetchAllHouses } from '../controllers/fetchhouseController.ts';
+import { joinHouse } from '../controllers/joinhouseController.ts';
 import { authenticate } from '../middleware/authenticate.ts';
 
 router.post('/create', authenticate, createHouse);
+router.post('/join', authenticate, joinHouse);
 router.get('/info', authenticate, fetchHouseInfo);
 router.get('/list', authenticate, fetchAllHouses);
 
